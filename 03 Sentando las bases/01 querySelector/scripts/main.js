8a0a59f96107f6045057fdae03f15b50be11be1d
nataliafleitas <nataliafleitas2298@gmail.com>
2020-01-10 16:23:34 -0300

    Hice los ejercicios pedidos en el README

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,13 @@
 console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+const comentario = document.querySelector('span');
+comentario.style.color= 'red';
+
+const favorito = document.querySelector('.comment');
+favorito.classList.add('comment--text');
+
+
+const borde = document.querySelectorAll('hr');
+for(let i=0; i < borde.length;i++){
+    borde[i].style.borderColor= "red";
+};
+
