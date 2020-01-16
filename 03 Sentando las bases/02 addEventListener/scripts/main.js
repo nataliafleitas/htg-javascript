e5a820eb7447895632579c850a1e693e7c1fe738
nataliafleitas <nataliafleitas2298@gmail.com>
2020-01-16 16:49:55 -0300

    Complete todos los ejercicios

--- 03 Sentando las bases/02 addEventListener/scripts/main.js
+++ 03 Sentando las bases/02 addEventListener/scripts/main.js
@@ -2,15 +2,38 @@
 
 const formulario = document.querySelector('#contacto');
 const inputNombre = document.querySelector('#input-nombre');
+const mensaje = document.querySelector('.mensaje');
+const inputSolucion = document.querySelector('#input-solucion');
+const comentario = document.querySelector('.comentario');
 
 function recibirNombre(event) {
+  console.log('hola');
+  comentario.textContent = inputNombre.value+' '+' '+  inputSolucion.value;
   event.preventDefault();
-  console.log(event);
+  //formulario.textContent = 'Se envio los datos'; ESTO LO QUE HACIA ERA TRANSFORMAR
+  EL CAMPO INPUT EN TextDecoder, ENTONCES DESAPARECIA EL CAMPO.
+  //formulario.style.color = 'blue';
+
+  
+
 }
 
+
 function interceptarNombre(event) {
-  console.log(event.target.value);
+
+  const nombre = event.target.value;
+  if(nombre.length < 5 || nombre.length > 50 ){
+    mensaje.textContent = 'Supero la cantidad';
+    mensaje.style.color = "green";
+    console.log('no es valido');
+  }else{
+    mensaje.textContent='';
+
+    }  
 }
 
 formulario.addEventListener('submit', recibirNombre);
 inputNombre.addEventListener('input', interceptarNombre);
+mensaje.addEventListener('span', interceptarNombre); //No es necesario ponerlo, aca digo que es un evento "span"
+
+
